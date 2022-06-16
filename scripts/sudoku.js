749a9c6bebebd308d028dd24442957cc7bec72e8
ThinhLe881 <thinhleminh201@gmail.com>
2022-06-16 18:09:42 -0400

    Fix solved btn modal

--- scripts/sudoku.js
+++ scripts/sudoku.js
@@ -204,7 +204,7 @@ window.onload = function() {
     if (onClickCell) {
       onClickCell.classList.remove("on-click");
     }
-    won(true);
+    won(false);
     for (let i = 0, row; row = table.rows[i]; i++) {
       for (let j = 0, col; col = row.cells[j]; j++) {
         row.cells[j].classList.remove("const");
